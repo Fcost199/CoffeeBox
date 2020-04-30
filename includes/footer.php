cf68aa4b2e28a9150483bbec2d7ae83f74417a21
doube-du <eduardohppa@gmail.com>
2020-04-30 20:53:27 -0300

    adicionado slider

--- includes/footer.php
+++ includes/footer.php
@@ -1,3 +1,4 @@
 <footer>
 <!-- Conteúdo constante de rodapé aqui -->
         </footer>
+        <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>

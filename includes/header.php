c38562057449e8156b6e11d4e6505e071b1d4150
unknown <costasfernando199@gmail.com>
2020-05-01 19:30:37 -0300

    Header

--- includes/header.php
+++ includes/header.php
@@ -1,10 +1,14 @@
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">
-                <img src="./assets/logo-CoffeeBox.png" width="90" height="50" class="d-inline-block align-top" alt="">
-        </a>
+<nav class="navbar navbar-expand-lg navbar-light" style="margin-bottom: 10px;">
+        
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
         </button>
+        <a class="navbar-brand" href="#">
+                <img src="./assets/logo-CoffeeBox.png" width="90" height="50" class="d-inline-block align-top" alt="">
+        </a>
+        <a class="navbar-brand" href="#">
+                <img src="./assets/icons/carrinho.png" width="50" height="50" class="d-inline-block align-top" alt="">
+        </a>
         <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav">
                         <li class="nav-item">
@@ -17,11 +21,12 @@
                                 <a class="nav-link" href="#">Sobre</a>
                         </li>
                         <li class="nav-item">
-                                <a class="nav-link" href="#">Carrinho</a>
-                        </li>
-                        <li class="nav-item">
                                 <a class="nav-link" href="#">Login</a>
                         </li>
                 </ul>
         </div>
+        
+
+        
+
 </nav>

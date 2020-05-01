d685eb2c8dc44939eb3f76d67caca0dcf4bf2c70
unknown <costasfernando199@gmail.com>
2020-05-01 18:58:06 -0300

    Header!

--- includes/footer.php
+++ includes/footer.php
@@ -1,8 +1,9 @@
 <footer>
 <!-- Conteúdo constante de rodapé aqui -->
         </footer>
-        <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+        <script type="text/javascript" src="./node_modules/jquery/dist/jquery.min.js"></script>
         <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+        <script type="text/javascript" src="node_modules/bootstrap/dist/js/bootstrap.min.js"></script>
         <script>
             //Sliders
             $('.banner').slick();
--- includes/header.php
+++ includes/header.php
@@ -1,12 +1,27 @@
-<header>
-        <div class="container">
-                <div class="row">
-                        <div class="col-sm-12 col-md-6 col-lg-6">
-
-                        </div>
-                        <div class="col-sm-12 col-md-6 col-lg-6">
-
-                        </div>
-                </div>
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <a class="navbar-brand" href="#">
+                <img src="./assets/logo-CoffeeBox.png" width="90" height="50" class="d-inline-block align-top" alt="">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                        <li class="nav-item">
+                                <a class="nav-link" href="#">Produtos</a>
+                        </li>
+                        <li class="nav-item">
+                                <a class="nav-link" href="#">Planos</a>
+                        </li>
+                        <li class="nav-item">
+                                <a class="nav-link" href="#">Sobre</a>
+                        </li>
+                        <li class="nav-item">
+                                <a class="nav-link" href="#">Carrinho</a>
+                        </li>
+                        <li class="nav-item">
+                                <a class="nav-link" href="#">Login</a>
+                        </li>
+                </ul>
         </div>
-</header>
+</nav>

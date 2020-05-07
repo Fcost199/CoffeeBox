347ebeb8b74483dbd6830e606917474fd3992dc4
unknown <eduardohppa@gmail.com>
2020-05-07 18:45:10 -0300

    'paginas'

---
+++ cadastrar.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+    <head>
+        <?php require('includes/head-tags.php'); ?>
+        
+        <title>CoffeBox - Seu Café em Casa</title>
+    </head>
+    <body>
+        <?php require('includes/header.php'); ?>
+        <main class="sua-classe">
+            <!-- Conteúdo aqui -->
+        </main>
+        <?php require('includes/footer.php'); ?>
+        
+    </body>
+</html>
--- planos.php
+++ planos.php
@@ -7,56 +7,71 @@
     </head>
     <body>
         <?php require('includes/header.php'); ?>
-        <main class="sua-classe">
-            <table>
-                <thead>
-                    <tr>
-                        <td>&nbsp;</td>
-                        <td>Plano Bronze</td>
-                        <td>Plano Prata</td>
-                        <td>Plano Ouro</td>
-                        <td>Plano Infinity</td>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                    <tr>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                        <td></td>
-                    </tr>
-                </tbody>
-            </table>
+        <main class="planos">
+            <div class="container">
+                <table class="table">
+                    <thead>
+                        <tr>
+                            <td><img src="assets/logo-CoffeeBox.png" width="150px"></td>
+                            <td><img src="assets/bronze.png" width="80px"><br>Plano Bronze</td>
+                            <td><img src="assets/prata.png" width="80px"><br>Plano Prata</td>
+                            <td><img src="assets/ouro.png" width="80px"><br>Plano Ouro</td>
+                            <td><img src="assets/infinity.png" width="80px"><br>Plano Infinity</td>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>Diferencial 1</td>
+                            <td><i class="fas fa-check-circle"></i></td>
+                            <td><i class="fas fa-check-circle"></i></td>
+                            <td><i class="fas fa-check-circle"></i></td>
+                            <td><i class="fas fa-check-circle"></i></td>
+                        </tr>
+                        <tr>
+                            <td>Diferencial 2</td>
+                            <td></td>
+                            <td><i class="fas fa-check-circle"></i></td>
+                            <td><i class="fas fa-check-circle"></i></td>
+                            <td><i class="fas fa-check-circle"></i></td>
+                        </tr>
+                        <tr>
+                            <td>Diferencial 3</td>
+                            <td></td>
+                            <td></td>
+                            <td><i class="fas fa-check-circle"></i></td>
+                            <td><i class="fas fa-check-circle"></i></td>
+                        </tr>
+                        <tr>
+                            <td>Diferencial 4</td>
+                            <td></td>
+                            <td></td>
+                            <td><i class="fas fa-check-circle"></i></td>
+                            <td><i class="fas fa-check-circle"></i></td>
+                        </tr>
+                        <tr>
+                            <td>Diferencial 5</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td><i class="fas fa-check-circle"></i></td>
+                        </tr>
+                        <tr>
+                            <td>Diferencial 6</td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td><i class="fas fa-check-circle"></i></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td><a href="plano.php" class="btn btn-dark w-100">Ver Plano</a></td>
+                            <td><a href="plano.php" class="btn btn-dark w-100">Ver Plano</a></td>
+                            <td><a href="plano.php" class="btn btn-dark w-100">Ver Plano</a></td>
+                            <td><a href="plano.php" class="btn btn-dark w-100">Ver Plano</a></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
         </main>
         <?php require('includes/footer.php'); ?>
         
